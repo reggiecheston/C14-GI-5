de3ae3bde86619fc41a63207ae6554039eb62b59
Reggie Cheston <reggiecheston@AdminisatorsMBP.lan>
2023-10-20 16:42:43 -0400

    updates scripts with comments, updates README

--- scripts.js
+++ scripts.js
@@ -6,16 +6,21 @@ let output = "";
 const calculate = (btnValue) => {
 
     if(btnValue === "="  && output !== "") {
+        // converts to a percentage
         output = eval(output.replace("%", "/100"));
     } else if(btnValue === "C") {
+        // Clears display
         output = "";
     } else if(btnValue === "DEL") {
+        // Deletes last character
         output = output.toString().slice(0, -1);
     } else {
+        // concatenates input
         if (output === "" && special.includes(btnValue)) return;
         output += btnValue;
     }
 
+    // Handles dividing by 0
     if (output == "Infinity") {
         output = "undefined";
     }
@@ -27,6 +32,6 @@ const calculate = (btnValue) => {
 
 // add event listener to buttons
 button.forEach((button) => {
-    // button click calls calculate() (change console.log to calculate)
+    // button click calls calculate()
     button.addEventListener("click", e => calculate(e.target.dataset.value));
 })
