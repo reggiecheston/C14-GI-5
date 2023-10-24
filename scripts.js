c7ec4ddb8674a682bab498e55d7a191ae4b6c3eb
Reggie Cheston <reggiecheston@Administrators-MacBook-Pro.local>
2023-10-24 15:49:06 -0400

    removes redundancy

--- scripts.js
+++ scripts.js
@@ -1,37 +1,32 @@
 const displayOutput = document.querySelector(".display--output");
 const button = document.querySelectorAll(".grid__item--button");
-let special = ["%", "/", "*", "-", "+","="];
 let output = "";
 
-const calculate = (btnValue) => {
+calculate = (btnValue) => {
+  // output
+  if (btnValue === "=" && output !== "") {
+    // converts to a percentage
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "C") {
+    // Clears display
+    output = "";
+  } else if (btnValue === "DEL") {
+    // Deletes last character
+    output = output.toString().slice(0, -1);
+  } else {
+    output += btnValue;
+  }
 
-    if(btnValue === "="  && output !== "") {
-        // converts to a percentage
-        output = eval(output.replace("%", "/100"));
-    } else if(btnValue === "C") {
-        // Clears display
-        output = "";
-    } else if(btnValue === "DEL") {
-        // Deletes last character
-        output = output.toString().slice(0, -1);
-    } else {
-        // concatenates input
-        if (output === "" && special.includes(btnValue)) return;
-        output += btnValue;
-    }
+  // Handles dividing by 0
+  if (output == "Infinity") {
+    output = "undefined";
+  }
 
-    // Handles dividing by 0
-    if (output == "Infinity") {
-        output = "undefined";
-    }
-
-    displayOutput.value = output;
-    
-}
-
+  displayOutput.value = output;
+};
 
 // add event listener to buttons
 button.forEach((button) => {
-    // button click calls calculate()
-    button.addEventListener("click", e => calculate(e.target.dataset.value));
-})
+  // button click calls calculate()
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
